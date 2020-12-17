dcafca7f65fe68d1d994d615c12dba07a314b55f
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-17 17:22:11 +0530

    added dif in br1

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,4 +5,7 @@ public class Calc {
 	public void add(int n1,int n2) {
 		System.out.println("sum= "+(n1+n2));
 	}
+	public void diff(int n1,int n2) {
+		System.out.println("diif = "+(n1-n2));
+	}
 }
