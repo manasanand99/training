ff1ea28910dffb6a267487493d6521901fc62c4b
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-17 17:46:22 +0530

    added sub in cal from br1

---
+++ DemoApp/src/com/lti/model/Calc.java
@@ -0,0 +1,11 @@
+package com.lti.model;
+
+public class Calc {
+
+	public void add(int n1,int n2) {
+		System.out.println(n1+n2);
+	}
+	public void sub(int n1,int n2) {
+		System.out.println(n1-n2);
+	}
+}
