b74ed3469a145d3ca7e4f8d3e78609299df2cc80
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-18 15:43:47 +0530

    removed sub and mul

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,11 +5,5 @@ public class Calc {
 	public void add(int n1,int n2) {
 		System.out.println("sum= "+(n1+n2));
 	}
-	public void diff(int n1,int n2) {
-		System.out.println("diif = "+(n1-n2));
-	}
-	public void mul(int n1,int n2) {
-		System.out.println("mul ="+n1*n2);
-		System.out.println("lti");
-	}
+	
 }
