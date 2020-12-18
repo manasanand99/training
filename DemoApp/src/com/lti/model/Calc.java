d42e01fdca5cf39d8971ca7bcd028fd3e66a0025
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-18 13:33:17 +0530

    added mul function and area class

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -3,11 +3,13 @@ package com.lti.model;
 public class Calc {
 
 	public void add(int n1,int n2) {
-
-		System.out.println("sum= "(n1+n2));
+		System.out.println("sum= "+(n1+n2));
 	}
 	
 	public void diff(int n1,int n2) {
 		System.out.println("diif = "+(n1-n2));
+	}
+	public void mul(int n1,int n2) {
+		System.out.println(n1*n2);
 	}
 }
